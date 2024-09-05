b3ea5f32007a2f051d2e7c5ca11d06e8c620357c
Zdunn-Design <zdunn16934@gmail.com>
2024-09-04 20:58:31 -0400

    Adding constants config file

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
 
+from constants import *
+
 def main():
+
     print("Starting asteroids!")
+    print(f"Screen width: " + str(SCREEN_WIDTH))
+    print(f"Screen height: " + str(SCREEN_HEIGHT))
 
 if __name__ == "__main__":
     main()
