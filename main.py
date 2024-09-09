319ff77645f89cb0d424ae07d27aec900ea01478
Zdunn-Design <zdunn16934@gmail.com>
2024-09-09 13:04:11 -0400

    update ignore and set up initial loop

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
-    print("Starting asteroids!")
-    print(f"Screen width: " + str(SCREEN_WIDTH))
-    print(f"Screen height: " + str(SCREEN_HEIGHT))
 
 if __name__ == "__main__":
     main()
